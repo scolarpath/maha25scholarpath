d963f3f897fd9c386e9893846376db69572935e4
scolarpath <srushti.kolawale.comp.2024@vpkbiet.org>
2026-05-16 12:59:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,8 +83,8 @@ def send_otp():
         return "OTP sent successfully"
 
     except Exception as e:
-        print("SENDGRID ERROR:", str(e))
-        return f"Error sending OTP: {str(e)}"
+    print("SENDGRID FULL ERROR:", repr(e))
+    return str(e)
 
 # ---------------- VERIFY OTP ----------------
 @app.route("/verify_otp", methods=["POST"])
